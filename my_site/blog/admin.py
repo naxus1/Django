202400e63ca035ef2358b809c7bc8d978c398bc0
naxus1 <naxus1@gmail.com>
2019-11-15 16:27:45 -0500

    [add] it was added filter, prepopulated to admin post

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -6,4 +6,8 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish',
                     'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    prepopulated_fields = {'slug': ('title', ) }
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
 
