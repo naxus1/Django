496157dc9682ef7d921f1a466a537bd75fc08a03
naxus1 <naxus1@gmail.com>
2019-11-19 21:45:35 -0500

    [add] It was added urlpatters for post_list and post_detail

---
+++ my_site/blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    #post views
+    path('', views.post_list, name='post_list'),
+    path('<int:yer>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
+        name='post_detail')
+]
+
+
+
+
