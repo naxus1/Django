aab80856df5ddec438fe729b1979a061214c12b7
naxus1 <naxus1@gmail.com>
2019-11-19 21:23:23 -0500

    [add] It was added the views post detail in views

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -7,3 +7,14 @@ def post_list(request):
     return render(request,
                 'blog/post/list.html',
                 {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                                    status='published',
+                                    publish__year=year,
+                                    publish__month=month,
+                                    publish__day=day)
+    return render(request,
+                'blog/post/detail.html',
+                {'post': post})
