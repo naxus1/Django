83d508b11b6c84775101ab01c6105c433726fd65
naxus1 <naxus1@gmail.com>
2019-11-19 22:02:33 -0500

    [add] It was modified the models for tha view  post_detail

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 # Model post
 class Post(models.Model):
@@ -27,4 +28,11 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
-                
+
+def get_absolute_url(self):
+    return reverse("blog:post_detail",
+                    args=[self.publish.year,
+                            self.publish.month,
+                            self.publish.day,
+                            self.slug])
+              
