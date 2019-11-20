68b83d2df7d36a362a9d91d03235208eae12fc7e
naxus1 <naxus1@gmail.com>
2019-11-19 21:15:54 -0500

    [Add] - It was created the view for all post

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request,
+                'blog/post/list.html',
+                {'posts': posts})
