4fa1050d87c425f5f2b3205918781682d2d01e87
naxus1 <naxus1@gmail.com>
2019-11-19 20:56:28 -0500

    [add] It was ordered to model post and search fields

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -7,7 +7,9 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish',
                     'status')
     list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title', ) }
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
 
