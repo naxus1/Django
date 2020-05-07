366b9491efec817fcc3b24ae3b93381f5ac738c4
naxus1 <naxus1@gmail.com>
2020-05-07 18:22:00 -0500

    [Deleted] It was deleted folder taskmate

--- taskmate/todolist_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from todolist_app.models import TaskList
-
-# Register your models here.
-admin.site.register(TaskList)
--- taskmate/todolist_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TodolistAppConfig(AppConfig):
-    name = 'todolist_app'
